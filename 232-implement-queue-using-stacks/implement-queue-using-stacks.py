ff201341aa0854fc1e8056406619d56112203917
Haripriya Tripathi <197859487+haripriyatripathi@users.noreply.github.com>
2026-06-29 00:28:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (27.85%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,39 @@
+class MyQueue(object):
+
+    def __init__(self):
+        # stack for push
+        self.in_stack = []
+
+        # stack for pop/peek
+        self.out_stack = []
+
+    def push(self, x):
+        # push into input stack
+        self.in_stack.append(x)
+
+    def pop(self):
+        # move elements if needed
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+
+        return self.out_stack.pop()
+
+    def peek(self):
+        # move elements if needed
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+
+        return self.out_stack[-1]
+
+    def empty(self):
+        return not self.in_stack and not self.out_stack
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
